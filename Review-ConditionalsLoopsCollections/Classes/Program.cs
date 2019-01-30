3f2a85350cffef2dc47408a25cdac70f979444cb
Alyssa Baker <Alyssa.B.Baker-1@ou.edu>
2019-01-30 11:15:42 -0600

    classes (Student)

---
+++ Review-ConditionalsLoopsCollections/Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world.");
+            Student alyssa = new Student();
+            alyssa.FirstName = "Alyssa";
+            alyssa.LastName = "Baker";
+            Student tim = new Student(3, "Timothy", "Fisher", 80000.5);
+
+            Console.ReadKey();
+        }
+    }
+}
